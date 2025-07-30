79301e71fca1ec5f884b7d2d041df4ec2dbf00d2
TheYonk613 <143032439+TheYonk613@users.noreply.github.com>
2025-07-30 22:16:44 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
 import { serveDir } from "https://deno.land/std/http/file_server.ts";
 
-Deno.serve((req) => serveDir(req, { fsRoot: "./public" }));
+Deno.serve((req) => serveDir(req, { fsRoot: "." }));
