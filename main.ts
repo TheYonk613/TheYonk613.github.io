f47bfca1b18d889d22813b763ef2f2bac55a2069
TheYonk613 <NetanelYounker@gmail.com>
2025-08-11 16:50:00 +0300

    Add simple Deno server to serve static files properly

---
+++ main.ts
@@ -0,0 +1,4 @@
+import { serve } from "https://deno.land/std@0.208.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.208.0/http/file_server.ts";
+
+await serve((req) => serveDir(req, { fsRoot: "." }));
